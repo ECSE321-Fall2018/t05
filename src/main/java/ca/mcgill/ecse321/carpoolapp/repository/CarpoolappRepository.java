58917cf6b483296abbb251cd59df371b9d7bd8e1
alexnorms <alexa.normandin@mail.mcgill.ca>
2018-10-06 13:55:57 -0400

    Added all JUnit Jupiter dependencies and Plugins to pom.xml

--- src/main/java/ca/mcgill/ecse321/carpoolapp/repository/CarpoolappRepository.java
+++ src/main/java/ca/mcgill/ecse321/carpoolapp/repository/CarpoolappRepository.java
@@ -1,10 +1,22 @@
 package ca.mcgill.ecse321.carpoolapp.repository;
 
+import java.util.ArrayList;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.carpoolapp.model.Ad;
+import ca.mcgill.ecse321.carpoolapp.model.Admin;
+import ca.mcgill.ecse321.carpoolapp.model.CarPoolManager;
+import ca.mcgill.ecse321.carpoolapp.model.Driver;
+import ca.mcgill.ecse321.carpoolapp.model.Passenger;
+import ca.mcgill.ecse321.carpoolapp.model.Stop;
+import ca.mcgill.ecse321.carpoolapp.model.User;
+import ca.mcgill.ecse321.carpoolapp.model.Vehicle;
+
 
 @Repository
 public class CarpoolappRepository {
@@ -26,4 +38,40 @@ public class CarpoolappRepository {
 //		return participant;
 //	}
 
+	
+	//AN
+	@Transactional
+	public User getUser(int id) {
+		User user = entityManager.find(User.class, id);
+		return user;	
+	}
+	
+	//AN
+	@Transactional 
+	public Ad getAd(int id) {
+		Ad ad = entityManager.find(Ad.class, id);
+		return ad;
+	}
+	
+	@Transactional
+	public User createUser(int id, String name, CarPoolManager carpoolManager) {
+		User user = new User(id, name, carpoolManager);
+		entityManager.persist(user);
+		return user;
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
