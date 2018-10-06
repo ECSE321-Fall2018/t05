237f65d99eb223110e96dfbbc0d8c68abd8cd2af
alexnorms <alexa.normandin@mail.mcgill.ca>
2018-10-06 14:28:26 -0400

    Added JUnit dependencies to pom.xml

--- src/test/java/ca/mcgill/ecse321/carpoolapp/CarpoolappApplicationTests.java
+++ src/test/java/ca/mcgill/ecse321/carpoolapp/CarpoolappApplicationTests.java
@@ -5,6 +5,27 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ca.mcgill.ecse321.carpoolapp.services.*;
+import ca.mcgill.ecse321.carpoolapp.model.User;
+import ca.mcgill.ecse321.carpoolapp.repository.CarpoolappRepository;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CarpoolappApplicationTests {
@@ -14,3 +35,24 @@ public class CarpoolappApplicationTests {
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
