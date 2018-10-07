9e18a36df5faeb69de6b9285579d1865e0890ef2
AHbelanger <antoine@Antoines-MacBook-Pro.local>
2018-10-07 17:38:36 -0400

    Sorting methods updated

--- src/main/java/ca/mcgill/ecse321/carpoolapp/services/CarPoolTest.java
+++ src/main/java/ca/mcgill/ecse321/carpoolapp/services/CarPoolTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
+import antlr.collections.List;
 import ca.mcgill.ecse321.carpoolapp.model.Ad;
 import ca.mcgill.ecse321.carpoolapp.model.Admin;
 import ca.mcgill.ecse321.carpoolapp.model.CarPoolManager;
@@ -153,26 +154,59 @@ class CarPoolTest
 		assertEquals(driver1, activeDrivers.get(0));
 		assertEquals(driver2, activeDrivers.get(1));	
 	}
-
+	
 	@Test
-	void getActivePassengers() 
+	void getTopDrivers() 
 	{
 		User user1 = service.createUser(101, "user1");
 		User user2 = service.createUser(102, "user2");
+		User user3 = service.createUser(103, "user3");
 		
 		Driver driver1 = service.createDriver(user1);
 		Driver driver2 = service.createDriver(user2);
+		Driver driver3 = service.createDriver(user3);
 		
-		Vehicle vehicle1 = service.createVehicle(2000, "bmw", "a1b2c3", 4, driver1);
-		Vehicle vehicle2 = service.createVehicle(2000, "bmw", "a1b2c3", 4, driver2);
+		driver1.setTotalDistance(50);
+		driver2.setTotalDistance(100);
+		driver3.setTotalDistance(10);
 		
-		Ad ad1 = service.createAd(driver1, 123, 5, vehicle1);
-		Ad ad2 = service.createAd(driver2, 123, 5, vehicle2);
+		ArrayList<Driver> sortedList = service.getTopDrivers();
+
 		
-		ArrayList<Driver> activeDrivers = service.getActiveDrivers();
-		
-		assertEquals(2, activeDrivers.size());
-		assertEquals(driver1, activeDrivers.get(0));
-		assertEquals(driver2, activeDrivers.get(1));	
+		assertEquals(3, sortedList.size());
+		assertEquals(driver1, sortedList.get(1));
+		assertEquals(driver2, sortedList.get(0));	
+		assertEquals(driver3, sortedList.get(2));	
 	}
+	
+	
+
+	@Test
+	void getTopPassengers() 
+	{
+		User user1 = service.createUser(101, "user1");
+		User user2 = service.createUser(102, "user2");
+		User user3 = service.createUser(103, "user3");
+		
+		Passenger passenger1 = service.createPassenger(user1);
+		Passenger passenger2 = service.createPassenger(user2);
+		Passenger passenger3 = service.createPassenger(user3);
+		
+		passenger1.setTotalDistance(50);
+		passenger2.setTotalDistance(100);
+		passenger3.setTotalDistance(10);
+		
+		ArrayList<Passenger> sortedList = service.getTopPassengers();
+		
+		assertEquals(3, sortedList.size());
+		assertEquals(passenger1, sortedList.get(1));
+		assertEquals(passenger2, sortedList.get(0));	
+		assertEquals(passenger3, sortedList.get(2));	
+	}
+	
+	@Test
+	void listAdsByStops() {
+		
+	}
+	
 }
