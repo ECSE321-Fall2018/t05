a85567b3c05324e27164d1d14115943e85d5f10f
alexnorms <alexa.normandin@mail.mcgill.ca>
2018-10-07 14:25:42 -0400

    Added to controller mapping

--- src/main/java/ca/mcgill/ecse321/carpoolapp/controller/CarpoolappController.java
+++ src/main/java/ca/mcgill/ecse321/carpoolapp/controller/CarpoolappController.java
@@ -1,5 +1,8 @@
 package ca.mcgill.ecse321.carpoolapp.controller;
 
+import java.sql.Date;
+import java.sql.Time;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,9 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.mcgill.ecse321.carpoolapp.model.Ad;
 import ca.mcgill.ecse321.carpoolapp.model.Admin;
 import ca.mcgill.ecse321.carpoolapp.model.Driver;
 import ca.mcgill.ecse321.carpoolapp.model.Passenger;
+import ca.mcgill.ecse321.carpoolapp.model.Stop;
+import ca.mcgill.ecse321.carpoolapp.model.Vehicle;
 import ca.mcgill.ecse321.carpoolapp.repository.CarpoolappRepository;
 
 @RestController
@@ -42,7 +48,7 @@ public class CarpoolappController {
 		return driver.getUser().getName();
 	}
 	
-	@GetMapping("/admin/{id}")
+	@GetMapping("/driver/{id}")
 	public String queryDrvier(@PathVariable("id") int id)
 	{
 		Driver driver = repository.getDriver(id);
@@ -69,9 +75,57 @@ public class CarpoolappController {
 		return passenger.getUser().getName();
 	}
 	
+	@PostMapping("/ad/{id}")
+	public int createAd(Driver driver, @PathVariable("id") int id, int price, Vehicle vehicle) {
+		
+		Ad ad = repository.createAd(driver, id, price, vehicle);
+		return ad.getId();
+	}
 	
+	@GetMapping("/ad/{id}")
+	public String queryAd(@PathVariable("id") int id) {
+		
+		Ad ad = repository.getAd(id);
+		if (ad == null) {
+			return "NOT FOUND";
+		}
+		return "Ad number" + ad.getId();
+	}
 	
+	@PostMapping("/stop/{id}")
+	public int createStop(Ad ad, Time time, Date date, int x, int y, @PathVariable("id") int id) {
+		
+		Stop stop = repository.createStop(ad, time, date, x, y, id);
+		return stop.getId();
+	}
 	
+	@GetMapping("/stop/{id}")
+	public String queryStop(@PathVariable("id") int id) {
+		
+		Stop stop = repository.getStop(id);
+		if (stop == null) {
+			return "NOT FOUND";
+		}
+		return "Ad number" + stop.getId();
+	}
+	
+	@PostMapping("/vehicle/{plateNumber}")
+	public String createVehicle(int year, String brand, @PathVariable("plateNumber") String plateNumber, int availableSeat, Driver driver) {
+		
+		Vehicle vehicle = repository.createVehicle(year, brand, plateNumber, availableSeat, driver);
+		return vehicle.getPlateNumber();
+	}
+	
+	@GetMapping("/vehicle/{plateNumber}")
+	public String queryVehicle(@PathVariable("plateNumber") String plateNumber) {
+		
+		Vehicle vehicle = repository.getVehicle(plateNumber);
+		
+		if (vehicle == null) {
+			return "NOT FOUND";
+		}
+		return vehicle.getPlateNumber();
+	}
 	
 //	@PostMapping("/participants/{name}")
 //	public String createParticipant(@PathVariable("name") String name) {
