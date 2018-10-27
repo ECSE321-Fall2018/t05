d86f3d3916a306aa907c6e5f75d190d1e4e748f7
alexnorms <alexa.normandin@mail.mcgill.ca>
2018-10-27 19:23:01 -0400

    Created Login and Create trip in Driver UI

---
+++ Android-Driver/app/src/main/java/ca/mcgill/ecse321/android_driver/LoginActivity.java
@@ -0,0 +1,26 @@
+package ca.mcgill.ecse321.android_driver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view){
+
+
+
+    }
+
+    public void signUp(View view){
+
+
+
+    }
+}
