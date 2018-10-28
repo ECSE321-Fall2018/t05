4987d1d971e1bca1a6ff59e84a5e1f914a1505b8
AHbelanger <antoine@Antoines-MacBook-Pro.local>
2018-10-28 16:37:09 -0400

    Driver interface layout completed

---
+++ Android-Driver/app/src/main/java/ca/mcgill/ecse321/android_driver/SignupActivity.java
@@ -0,0 +1,17 @@
+package ca.mcgill.ecse321.android_driver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void addDriver(View view) {
+    }
+}
