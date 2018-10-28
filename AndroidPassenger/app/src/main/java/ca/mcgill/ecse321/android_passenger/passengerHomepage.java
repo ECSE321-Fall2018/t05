c56748ddbca4a2da09c6a316bcdecb98aed573dc
alexnorms <alexa.normandin@mail.mcgill.ca>
2018-10-28 19:37:58 -0400

    Added to Passenger UI

---
+++ AndroidPassenger/app/src/main/java/ca/mcgill/ecse321/android_passenger/passengerHomepage.java
@@ -0,0 +1,21 @@
+package ca.mcgill.ecse321.android_passenger;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class passengerHomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_homepage);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
