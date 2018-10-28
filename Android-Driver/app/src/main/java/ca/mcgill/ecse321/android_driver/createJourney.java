d84c1bb0a7becdd970a7b712052e560c806343f7
alexnorms <alexa.normandin@mail.mcgill.ca>
2018-10-28 16:31:23 -0400

    Create a trip visuals complete

---
+++ Android-Driver/app/src/main/java/ca/mcgill/ecse321/android_driver/createJourney.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse321.android_driver;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class createJourney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_journey);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
