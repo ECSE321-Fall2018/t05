c9e06159b37ce27acc92ce6959ba10ff69a720a2
AHbelanger <hbelanger.antoine@gmail.com>
2018-10-30 22:44:35 -0400

    All passenger interfaces completed with partial intents

--- AndroidPassenger/app/src/main/java/ca/mcgill/ecse321/android_passenger/MainActivity.java
+++ AndroidPassenger/app/src/main/java/ca/mcgill/ecse321/android_passenger/MainActivity.java
@@ -1,7 +1,9 @@
 package ca.mcgill.ecse321.android_passenger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -11,10 +13,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_ads);
+        setContentView(R.layout.activity_login);
 
 
     }
 
 
+    public void login(View view){
+
+        Intent intent = new Intent(this, passengerHomepage.class);
+        startActivity(intent);
+
+    }
+
+    public void signup(View view){
+        Intent intent = new Intent(this, signup.class);
+        startActivity(intent);
+    }
+
 }
+
+
